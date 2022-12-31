a265e02ee60503eabf1f8026606ef66f4e5d8e55
ismail can yıldırım <ismailcan5106@gmail.com>
2022-12-31 10:52:41 +0300

    Temassız seviye seösörü için pin kodları eklendi

--- Marlin/src/gcode/control/M1071.cpp
+++ Marlin/src/gcode/control/M1071.cpp
@@ -12,6 +12,8 @@
 #include "../../lcd/extui/nextion/FileNavigator.h"
 
 int PDD1= PD1;
+int PDD2= PD0;
+int PDD3= PD14;
 
 void GcodeSuite::M1071()
 {   
@@ -24,6 +26,28 @@ void GcodeSuite::M1071()
     if(digitalRead(PDD1)== LOW){
         SERIAL_ECHOPGM("\xFF\xFF\xFF");
         SERIAL_ECHOPGM("pdd1.val=0");
+        SERIAL_ECHOPGM("\xFF\xFF\xFF");
+    }
+    pinMode(PDD2, INPUT_PULLDOWN);
+    if (digitalRead(PDD2)== HIGH){
+        SERIAL_ECHOPGM("\xFF\xFF\xFF");
+        SERIAL_ECHOPGM("pdd2.val=1");  
+        SERIAL_ECHOPGM("\xFF\xFF\xFF");
+    }
+    if(digitalRead(PDD2)== LOW){
+        SERIAL_ECHOPGM("\xFF\xFF\xFF");
+        SERIAL_ECHOPGM("pdd2.val=0");
+        SERIAL_ECHOPGM("\xFF\xFF\xFF");
+    }
+    pinMode(PDD3, INPUT_PULLDOWN);
+    if (digitalRead(PDD3)== HIGH){
+        SERIAL_ECHOPGM("\xFF\xFF\xFF");
+        SERIAL_ECHOPGM("pdd3.val=1");  
+        SERIAL_ECHOPGM("\xFF\xFF\xFF");
+    }
+    if(digitalRead(PDD3)== LOW){
+        SERIAL_ECHOPGM("\xFF\xFF\xFF");
+        SERIAL_ECHOPGM("pdd3.val=0");
         SERIAL_ECHOPGM("\xFF\xFF\xFF");
     }
 
