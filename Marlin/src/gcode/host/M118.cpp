31a0404377f03448bb233c404b0f5c4089c74e0d
ismail can yıldırım <ismailcan5106@gmail.com>
2023-01-07 11:01:59 +0300

    m118 ve m1181 dosyaları değişti m118 daha stabil hale geçirildi m1181 e ise wifi yenileme ile ilgili kod eklendi

--- Marlin/src/gcode/host/M118.cpp
+++ Marlin/src/gcode/host/M118.cpp
@@ -37,29 +37,8 @@ int x = 0;
 int y = 0;
 int z = 0;
 void GcodeSuite::M118() {
-  bool hasE = false, hasA = false;
-  #if HAS_MULTI_SERIAL
-    int8_t port = -1; // Assume no redirect
-  #endif
-  char *p = parser.string_arg;/*
-  for (uint8_t i = 3; i--;) {
-    // A1, E1, and Pn are always parsed out
-    if (!( ((p[0] == 'A' || p[0] == 'E') && p[1] == '1') || (p[0] == 'P' && NUMERIC(p[1])) )) break;
-    switch (p[0]) {
-      case 'A': hasA = true; break;
-      case 'E': hasE = true; break;
-      #if HAS_MULTI_SERIAL
-        case 'P': port = p[1] - '0'; break;
-      #endif
-    }
-    p += 2;
-    while (*p == ' ') ++p;
-  }*/
 
-  PORT_REDIRECT(WITHIN(port, 0, NUM_SERIAL) ? (port ? SERIAL_PORTMASK(port - 1) : SerialMask::All) : multiSerial.portMask);
-
-  if (hasE) SERIAL_ECHO_START();
-  if (hasA) SERIAL_ECHOPGM("//");
+  char *p = parser.string_arg;
 
   if (p[0] == 'A'){
     y = 1;
@@ -77,63 +56,18 @@ void GcodeSuite::M118() {
     x = 0;
   }
   if(!(p[0]=='A') && !(p[0]=='C') && !(z == 1)){
-    if (x==8){
-      SERIAL_ECHOPGM("\xFF\xFF\xFF");
-      SERIAL_ECHOPGM("b108.txt=\"","",p,"\"");
-      SERIAL_ECHOPGM("\xFF\xFF\xFF");
-      x+=1;
-    }
-    if (x==7){
-      SERIAL_ECHOPGM("\xFF\xFF\xFF");
-      SERIAL_ECHOPGM("b107.txt=\"","",p,"\"");
-      SERIAL_ECHOPGM("\xFF\xFF\xFF");
-      x+=1;
-    }
-    if (x==6){
-      SERIAL_ECHOPGM("\xFF\xFF\xFF");
-      SERIAL_ECHOPGM("b106.txt=\"","",p,"\"");
-      SERIAL_ECHOPGM("\xFF\xFF\xFF");
-      x+=1;
-    }
-    if (x==5){
-      SERIAL_ECHOPGM("\xFF\xFF\xFF");
-      SERIAL_ECHOPGM("b105.txt=\"","",p,"\"");
-      SERIAL_ECHOPGM("\xFF\xFF\xFF");
-      x+=1;
-    }
-    if (x==4){
-      SERIAL_ECHOPGM("\xFF\xFF\xFF");
-      SERIAL_ECHOPGM("b104.txt=\"","",p,"\"");
-      SERIAL_ECHOPGM("\xFF\xFF\xFF");
-      x+=1;
-    }
-    if (x==3){
-      SERIAL_ECHOPGM("\xFF\xFF\xFF");
-      SERIAL_ECHOPGM("b103.txt=\"","",p,"\"");
-      SERIAL_ECHOPGM("\xFF\xFF\xFF");
-      x+=1;
-    }
-    if (x==2){
-      SERIAL_ECHOPGM("\xFF\xFF\xFF");
-      SERIAL_ECHOPGM("b102.txt=\"","",p,"\"");
-      SERIAL_ECHOPGM("\xFF\xFF\xFF");
-      x+=1;
-    }
-    if (x==1){
-      SERIAL_ECHOPGM("\xFF\xFF\xFF");
-      SERIAL_ECHOPGM("b101.txt=\"","",p,"\"");
-      SERIAL_ECHOPGM("\xFF\xFF\xFF");
-      x+=1;
-    }
-    if (x==0){
-      SERIAL_ECHOPGM("\xFF\xFF\xFF");
-      SERIAL_ECHOPGM("b100.txt=\"","",p,"\"");
-      SERIAL_ECHOPGM("\xFF\xFF\xFF");
-      x+=1;
-    }
-
+    SERIAL_ECHOPGM("\xFF\xFF\xFF");
+    if (x == 0){SERIAL_ECHOPGM("b100.txt=\"");}
+    if (x == 1){SERIAL_ECHOPGM("b101.txt=\"");}
+    if (x == 2){SERIAL_ECHOPGM("b102.txt=\"");}
+    if (x == 3){SERIAL_ECHOPGM("b103.txt=\"");}
+    if (x == 4){SERIAL_ECHOPGM("b104.txt=\"");}
+    if (x == 5){SERIAL_ECHOPGM("b105.txt=\"");}
+    if (x == 6){SERIAL_ECHOPGM("b106.txt=\"");}
+    if (x == 7){SERIAL_ECHOPGM("b107.txt=\"");}
+    if (x == 8){SERIAL_ECHOPGM("b108.txt=\"");}
+    SERIAL_ECHOPGM(p);
+    SERIAL_ECHOPGM("\"\xFF\xFF\xFF");
+    x+=1;
   }
-
-  
-
 }
